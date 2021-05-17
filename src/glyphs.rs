23c930e0f336c691471d5a06b108558fdca9d127
Fredrick Brennan <copypaste@kittens.ph>
2021-05-16 20:57:59 -0400

    Properly support multiple encoded glyphs

--- src/glyphs.rs
+++ src/glyphs.rs
@@ -1,9 +1,9 @@
 use norad::{Font, Glyph};
-use unic_ucd::name::Name;
 use unic_ucd::category::GeneralCategory;
+use unic_ucd::name::Name;
 
 fn codepoints_to_string(cps: &Vec<char>) -> String {
-    let mut ret = String::with_capacity((cps.len()*4)+cps.len());
+    let mut ret = String::with_capacity((cps.len() * 4) + cps.len());
     for (i, cp) in cps.iter().enumerate() {
         ret.push_str(&format!("{:04x}", *cp as u32));
         if i != cps.len() - 1 {
@@ -17,7 +17,7 @@ fn name_to_string(name: &Name) -> String {
     match name {
         Name::NR1(c) => format!("HANGUL SYLLABLE {}", c),
         Name::NR2(s, c) => format!("{} {:06x}", s, *c as u32),
-        Name::NR3(sl) => format!("{}", sl.join(" "))
+        Name::NR3(sl) => format!("{}", sl.join(" ")),
     }
 }
 
@@ -25,8 +25,24 @@ use std::sync::Arc;
 fn print_glyph(g: Arc<Glyph>) {
     print!("{}\t{}\t", &g.name, codepoints_to_string(&g.codepoints));
     if g.codepoints.len() > 0 {
-        print!("{}\t", Name::of(g.codepoints[0]).map(|n|name_to_string(&n)).unwrap_or("<unencoded>".into()));
-        print!("{:?}\t", GeneralCategory::of(g.codepoints[0]));
+        print!(
+            "{}\t",
+            (g.codepoints
+                .iter()
+                .map(|cp| Name::of(*cp)
+                    .map(|n| name_to_string(&n))
+                    .unwrap_or("<unencoded>".into()))
+                .collect::<Vec<String>>())
+            .join(",")
+        );
+        print!(
+            "{}\t",
+            (g.codepoints
+                .iter()
+                .map(|cp| format!("{:?}", GeneralCategory::of(*cp))))
+            .collect::<Vec<String>>()
+            .join(",")
+        );
     }
     println!("")
 }
