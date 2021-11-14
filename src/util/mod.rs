c158a1bd18a485ac62d93bec13b9f65e3e794e7f
Fredrick Brennan <copypaste@kittens.ph>
2021-11-14 14:33:19 -0500

    Add missing file

---
+++ src/util/mod.rs
@@ -0,0 +1,77 @@
+#![allow(unused)]
+
+#[derive(Debug)]
+enum ValidatorRange {
+    All,
+    PositiveNotZero,
+    Positive,
+}
+
+fn arg_validator_f64_impl(v: String, range: ValidatorRange) -> Result<(), String> {
+    match v.parse::<f64>() {
+        Ok(i) => {
+            let err = || {
+                Err(String::from(&format!(
+                    "Value outside allowed range ({:?})",
+                    range
+                )))
+            };
+            match range {
+                ValidatorRange::All => Ok(()),
+                ValidatorRange::PositiveNotZero => {
+                    if i >= 0.0 {
+                        Ok(())
+                    } else {
+                        err()
+                    }
+                }
+                ValidatorRange::Positive => {
+                    if i > 0.0 {
+                        Ok(())
+                    } else {
+                        err()
+                    }
+                }
+            }
+        }
+        Err(_) => Err(String::from("Value must be a float")),
+    }
+}
+
+pub fn arg_validator_positive_f64(v: String) -> Result<(), String> {
+    arg_validator_f64_impl(v, ValidatorRange::Positive)
+}
+
+pub fn arg_validator_positive_or_zero_f64(v: String) -> Result<(), String> {
+    arg_validator_f64_impl(v, ValidatorRange::PositiveNotZero)
+}
+
+pub fn arg_validator_f64(v: String) -> Result<(), String> {
+    arg_validator_f64_impl(v, ValidatorRange::All)
+}
+
+pub fn arg_validator_isize(v: String) -> Result<(), String> {
+    match v.parse::<isize>() {
+        Ok(_) => Ok(()),
+        Err(_) => Err(String::from("Value must be an integer")),
+    }
+}
+
+pub fn arg_validator_usize(v: String) -> Result<(), String> {
+    match v.parse::<isize>() {
+        Ok(_) => Ok(()),
+        Err(_) => Err(String::from("Value must be a positive integer")),
+    }
+}
+
+pub fn arg_validator_suffix(
+    f: &impl Fn(String) -> Result<(), String>,
+    suffix: char,
+) -> impl Fn(String) -> Result<(), String> + '_ {
+    move |mut v| {
+        while v.ends_with(suffix) {
+            assert_eq!(v.pop().unwrap(), suffix);
+        }
+        f(v)
+    }
+}
